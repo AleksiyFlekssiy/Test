be535aa64077225c54771e2cc81baeb3f9a44e24
AleksiyFlekssiy <alex77y71@gmail.com>
2023-02-27 21:12:56 +0300

    Redacted sout.java

--- src/sout.java
+++ src/sout.java
@@ -1,7 +1,7 @@
 class sout{
     public static void main(String[] args) {
         World world = new World();
-        System.out.println("Hello World!");
+        System.out.println("ZA WARUDO");
         System.out.println(world.message);
 
     }
